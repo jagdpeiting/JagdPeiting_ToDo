383ceb7984de11e675fbb2cae3866802fd6fb756
sa3567 <sa3567@mci4me.at>
2024-02-06 19:58:35 +0100

    Update

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBLFsDiOXo0ZcJ9ICpNt_zL1U0Jpy9lFbc",
+  authDomain: "jagdpeitingtodo.firebaseapp.com",
+  projectId: "jagdpeitingtodo",
+  storageBucket: "jagdpeitingtodo.appspot.com",
+  messagingSenderId: "983594231285",
+  appId: "1:983594231285:web:1d273652af89492e626cbc",
+  measurementId: "G-SFX2N1L06Y"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 const db = firebase.database();
 const jagdstaendeListe = document.getElementById("jagdstaende");
 const addStandBtn = document.getElementById("addStandBtn");
